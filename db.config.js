e64bc75b95dddbcad03bafcf8ab084d6f65bf6fb
letsbuildwebsite <y59682258@gmail.com>
2023-11-04 01:05:58 +0800

    second

--- db.config.js
+++ db.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  DBTYPE:"oracle",                    //oracle|| mysql || mssql || postgresql
+  DBTYPE:"mssql",                   
   HOST: 'localhost',
-  USER: "SYSTEM",                    //root(default) for mysql|| SYSTEM(default) for oracle || sa for mssql
-  PASSWORD: "your password",  // oracle_password!!Q11 for oracle || root for mssql ||
-  DB: "your db name",                    //mysqlDB, oracleDB, postgresDB, mssqlDB1
-  PORT:1521,                         //do not need for mysqlDB|| 1521 for oracle|| do not need or may need for mssql
-  SID:'orcl',                        
-  dialect: "oracle",                //oracle, mysql, postgresql, mssql
+  USER: "sa",                    
+  PASSWORD: "your password", 
+  DB: "your db name",
+  SID:'mssql',
+  dialect: "mssql",        
   pool: {
     max: 5,
     min: 0,
